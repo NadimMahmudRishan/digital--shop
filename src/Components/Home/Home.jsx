9eed636aba36c03797212022c2ac03ca6a7cad38
Rishan <98330109+NadimMahmudRishan@users.noreply.github.com>
2024-10-30 09:34:01 +0600

    Project setup & nav and banner section complete.

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Carousel from "../Carousel/Carousel";
+import Navbar from "../Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Carousel></Carousel>
+    </div>
+  );
+};
+
+export default Home;
